61c9df4ee8b40f836f57835308ebdd6f324f595c
DCShiomiYuji <shiomi@g.dreamcareer.co.jp>
2025-04-13 15:42:19 +0900

    1件取得まで

--- index.js
+++ index.js
@@ -29,12 +29,13 @@ async function testConnection() {
 
 // 一覧取得 - GET /users/skills
 app.get('/users/skills', async (req, res) => {
-    const exp = req.query.exp;
+    const {exp, level, userId} = req.query;
     let query = 'SELECT * FROM UserSkills';
     let params = [];
+    let conditions = []; // 検索条件を追加
 
     try {
-      // expクエリパラメータが指定されている場合、フィルタリングする
+      // exp のフィルタリング
       if (exp !== undefined) {
         const expValue = parseInt(exp);
         
@@ -44,13 +45,39 @@ app.get('/users/skills', async (req, res) => {
         }
         
         // 指定された経験年数以上のスキルをフィルタリング
-        query = 'SELECT * FROM UserSkills WHERE experience >= ?';
-        params = [expValue];
+        conditions.push('experience >= ?');
+        params.push(expValue);
+      }
+
+      // level のフィルタリング
+      if (level !== undefined) {
+        const levelValue = parseInt(level);
+        
+        // 数値に変換できない場合はエラー
+        if (isNaN(levelValue)) {
+          return res.status(400).json({ error: 'レベル level は数値で指定してください' });
+        }
+        
+        // 指定されたレベル以上のスキルをフィルタリング
+        conditions.push('level >= ?');
+        params.push(levelValue);
+      }
+
+      // userId のフィルタリング
+      if (userId !== undefined) {
+        // 指定されたユーザーIDのスキルをフィルタリング
+        conditions.push('user_id = ?');
+        params.push(userId);
+      }
+
+      // WEHERE 句の結合
+      if (conditions.length > 0) {
+        query += ' WHERE ' + conditions.join(' AND ');
       }
 
       // クエリパラメータがない場合は全件返す
       const [skills] = await pool.query(query, params);
-      res.status(200).json(skills);
+      return res.status(200).json(skills);
 
    } catch (error) {
       console.error('予期せぬエラー:', error);
@@ -58,16 +85,27 @@ app.get('/users/skills', async (req, res) => {
    }
 });
 
-// 1件取得 - GET /skills/:id
-app.get('/skills/:id', (req, res) => {
-    const skill = skills.find(s => s.id === req.params.id);
 
-    // ここにエラーを追加
-    if (!skill) {
-        return res.status(404).json({ error: 'スキルが見つかりません' });
+
+// 1件取得 - GET /skills/:id
+app.get('/users/skills/:id', async(req, res) => {
+    try {
+        const [rows] = await pool.query(
+          'SELECT * FROM UserSkills WHERE id = ?', 
+          [req.params.id]
+        );
+
+        const userSkill = rows[0]; // 取得したスキル情報
+
+        if (!userSkill) {
+            return res.status(404).json({ error: 'スキルが見つかりません' });
+        }
+
+        return res.status(200).json(userSkill);
+    } catch (error) {
+        console.error('予期せぬエラー:', error);
+        return res.status(500).json({ error: '予期せぬエラーが発生しました。システム管理者に問い合わせてください。' });
     }
-    
-    res.status(200).json(skill);
 });
 
 app.listen(3000, async() => {
