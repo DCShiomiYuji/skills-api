7a8a53340c36d5e3b2763876b3fe10114e8b90a2
DCShiomiYuji <shiomi@g.dreamcareer.co.jp>
2025-04-17 00:21:42 +0900

    feat: cors の追加

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors"); // CORS ミドルウェアをインポート
 const { testConnection } = require("./src/config/db");
 const userSkills = require("./src/users-skills/index");
+
+// CORSミドルウェアを適用
+app.use(cors({
+  origin: '*', // すべてのオリジンからのリクエストを許可（本番環境では適切に制限することを推奨）
+  methods: '*', // 許可するHTTPメソッド
+  allowedHeaders: '*' // 許可するヘッダー
+}));
 
 // リクエストBodyを取得するために必要
 app.use(express.json());
